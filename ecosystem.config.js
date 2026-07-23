097d6c69f9b7e505f81988c5f25cd68a290f2426
Andalefx <101069303+Andalefx@users.noreply.github.com>
2026-07-23 17:55:12 +0300

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,83 @@
+let _sys = require('./Global/Settings/_system.json');
+let botcuk = [
+    {
+name: "Mainframe",
+namespace: "ANDALEFX",
+script: 'andale.js',
+watch: false,
+exec_mode: "cluster",
+max_memory_restart: "2G",
+cwd: "./Server/Voucher"
+ },
+{
+    name: "Controller",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Controller"
+  },
+  {
+    name: "Statistics",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Statistics"
+  },
+  {
+    name: "Sync",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Sync"
+  },
+  {
+    name: "Security_I",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Guard_I"
+  },
+  {
+    name: "Security_II",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Guard_II"
+  },
+  {
+    name: "Distributors",
+    namespace: "ANDALEFX",
+    script: 'andale.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Distributors"
+  },
+
+  ]
+if(_sys.TOKENS.WELCOME.Active) {
+    botcuk.push(
+    {
+    name: "Voices",
+    namespace: "ANDALEFX",
+    script: 'Start.js',
+    watch: false,
+    exec_mode: "cluster",
+    max_memory_restart: "2G",
+    cwd: "./Server/Welcome"
+  }
+  )
+}
+module.exports = {
+  apps: botcuk
+};
